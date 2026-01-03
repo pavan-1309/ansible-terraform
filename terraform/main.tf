14a3e20983c32acd1d20a782dc116a23524148f5
pavan <pavan@gmail.com>
2026-01-03 10:32:04 +0530

    new updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,16 @@ resource "aws_instance" "web" {
   key_name = "my-keypair"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0a0ee2d6bfc36780f"]
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update
+    apt-get install -y python3
+  EOF
+  
   tags = {
     Environment = var.environment
     Role        = "web"
+    Name        = "${var.environment}-web-${count.index + 1}"
   }
 }
